c567d8067528a91e1fefcab2f4ada2310e48d6cc
Markus M. May <triplem@tu.archserver.org>
2011-03-17 23:13:12 +0100

    Add comments and cleaned code

--- gash-migration.sh
+++ gash-migration.sh
@@ -7,91 +7,128 @@
 # to $1.tmp), are shifted into an own git repository in a subdirectory 
 # ($1).
 #
+# In the following we assume server-core as the given repository, this
+# is to make the examples easier. We are showing just one package (abs)
+# with only one file (PKGBUILD). The .git directory is shown explicitly
+# to show, which directories are real git-repositories.
+# The given repository (server-core) is called super-repository, the 
+# sub-directories are packages in our case. Packages are then transferred
+# to own repositories after the migration.
+#
+# Directory structure before migration (server):
+# /srv/git/server-core.git/.git
+# /srv/git/server-core.git/.gitignore
+# /srv/git/server-core.git/abs
+# /srv/git/server-core.git/abs/PKGBUILD
+# ...
+#
+# Directory structure after migration (server):
+# /srv/git/server-core.git/.git
+# /srv/git/server-core.git/.gitignore
+# /srv/git/server-core.git/.gashlist
+#
+# /srv/git/server-core/abs
+# /srv/git/server-core/abs/.git
+# /srv/git/server-core/abs/.gitignore
+# /srv/git/server-core/abs/PKGBUILD
+#
+# The migration is done on a client, the original repository (server-core)
+# is copied to a temporary directory (server-core.tmp) to allow adoptions 
+# on the structure of the super-repository.
+#
+# The given repository is then used as the new super-repository. 
+# This super-repository will just contain a special file (.gashlist) 
+# which contains a list of all sub-repositories. All the history in this
+# repository is still left in the repository. This could be moved in a 
+# separate step.
+# The new base directory and the sub-repositories needs to get created 
+# on the server.
+#
 
+# The given paramater is the used repository
+repo=$1
+# Server, where git-directories are located on
 SERVER=hydrogen.archserver.org
+# Directory on the server, where git-repositories are located
 GIT_SERVER_BASE_DIR=/srv/git
 
 # check if the given repository name is not empty
-if [ "$1" == "" ]; then
-  echo "Please provide a valid repository name"
+if [ "$repo" == "" ]; then
+  echo "Please provide a valid repository name (you provided an empty name)"
 	exit 1
 fi
 
 # check if the given repository name is a directory
-if [ ! -d "$1" ]; then
-	echo "Please provide a valid repository name"
+if [ ! -d "$repo" ]; then
+	echo "Please provide a valid repository name (the given name is not a directory)"
 	exit 1
 fi
 
 # check if the given repository name is really a repository (git)
-if [ ! -d "$1/.git" ]; then
-	echo "Please provide a valid repository name"
+if [ ! -d "$repo/.git" ]; then
+	echo "Please provide a valid repository name (the given name is not a valid git repo)"
 	exit 1
 fi
 
-# local movement of the directory to temporary location
-# the repository (e.g. server-core is moved to a temporary 
-# location, e.g. server-core.tmp). The temporary location is
-# used to detected all sub-projects.
-# The repository is then used as the base directory for the new sub-
-# repositories. The "old" repository is then used as the new super-
-# repository.
-# The new base directory, the sub-repositories needs to get created 
-# on the server. The old repository (with .git as a suffix) is then 
-# used as the new super-repository.
-repo_name=$1
-tmp_repo_name=$repo_name.tmp
-
-mv $repo_name $tmp_repo_name
-mkdir $repo_name
-
-base_dir=$PWD/$repo_name
-
+# store base directory
+base_dir=$PWD
+repo_dir=$base_dir/$repo
 echo $base_dir
+
+# local movement of the directory to temporary location
+tmp_repo_name=$repo.tmp
+cp $repo $tmp_repo_name -rf
+
+# clean repository
+cd $repo
+git rm * -rf
 
 # determine all sub-repositories in the super-repository
 cd $tmp_repo_name
 
 # create the new project directory (eg. server-core, explicitly without 
 # .git-suffix) on the server, to store new sub-repositories.
-ssh $SERVER 'mkdir $GIT_SERVER_BASE_DIR/$1'
-
+ssh $SERVER 'mkdir $GIT_SERVER_BASE_DIR/$repo'
 
 # this should be more error prone, concerning spaces in names
 # and stuff like this.
-for f in * ; do
-  if [ ! -d "$f" ]; then
-		echo "given sub-repository is not a directory, cannot go further"
-		exit 1
+for subrepo in * ; do
+  # check if the subrepo is a directory, if not, continue
+  if [ ! -d "$subrepo" ]; then
+		echo "given sub-repository ($subrepo) is not a directory, cannot go further"
+		continue
 	fi
 
-	echo "base_dir/f: $base_dir/$f"
+  # clone repository into sub-repository
+	echo "base_dir/subrepo: $base_dir/$subrepo"
 	cd $base_dir
-	git clone --no-hardlinks ../$tmp_repo_name $f
+	git clone --no-hardlinks $tmp_repo_name $subrepo
 
-	cd $f
+  # cleanup sub-repository
+	cd $subrepo
 	echo $PWD
-	git filter-branch --subdirectory-filter $f HEAD -- -- all
+	git filter-branch --subdirectory-filter $subrepo HEAD -- -- all
 	git reset --hard
   git gc --aggressive
 	git prune
 
   # the new git repository needs to be created on the live system
   # create directory
-	ssh $SERVER 'mkdir $GIT_SERVER_BASE_DIR/$1/$f.git'
+	ssh $SERVER 'mkdir $GIT_SERVER_BASE_DIR/$repo/$subrepo.git'
 	# create repository
-	ssh $SERVER 'cd $GIT_SERVER_BASE_DIR/$1/$f.git && git --bare init'
+	ssh $SERVER 'cd $GIT_SERVER_BASE_DIR/$repo/$subrepo.git && git --bare init'
 
+	# remove remote from sub-repository and add new remote on remote
+	# server
   git remote rm origin
-	git remote add origin git@git.archserver.org:$1/$f
+	git remote add origin git@git.archserver.org:$repo/$subrepo
 	git push origin master
 
-	cd $base_dir
-	echo $f >> .gitignore
-	echo $f >> .gashlist
+  # add sub-repository to .gitignore and .gashlist in 
+  # super-repository
+	cd $repo_dir
+	echo $subrepo >> .gitignore
+	echo $subrepo >> .gashlist
 
 	exit 1
 done
-
-
-
